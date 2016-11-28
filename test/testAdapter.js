9beeabddf20e721fb6b1843343ded5fdd0f69afb
Ingo Fischer <github@fischer-ka.de>
2016-11-28 17:19:01 +0100

    make test working with adapter name parameter

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -17,7 +17,7 @@ function checkConnectionOfAdapter(cb, counter) {
         return;
     }
 
-    states.getState('system.adapter.nut.0.alive', function (err, state) {
+    states.getState('system.adapter.' + adapterName + '.0.alive', function (err, state) {
         if (err) console.error(err);
         if (state && state.val) {
             if (cb) cb();
