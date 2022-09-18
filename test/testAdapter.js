d3e4ded40c02e9d166d72251ff8f88da725e35ea
Ingo Fischer <github@fischer-ka.de>
2022-09-19 00:20:28 +0200

    * bring back the old testing in combination with new

---
+++ test/testAdapter.js
@@ -0,0 +1,128 @@
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+/*jshint expr: true*/
+const expect = require('chai').expect;
+const setup = require(__dirname + '/lib/setup');
+const tests = require('./lib/testcases');
+
+let objects = null;
+let states = null;
+let onStateChanged = null;
+//var onObjectChanged = null;
+let sendToID = 1;
+
+const adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.') + 1);
+
+function sendTo(target, command, message, callback) {
+    onStateChanged = function (id, state) {
+        if (id === 'messagebox.system.adapter.test.0') {
+            callback(state.message);
+        }
+    };
+
+    states.pushMessage('system.adapter.' + target, {
+        command:    command,
+        message:    message,
+        from:       'system.adapter.test.0',
+        callback: {
+            message: message,
+            id:      sendToID++,
+            ack:     false,
+            time:    (new Date()).getTime()
+        }
+    });
+}
+
+function checkConnectionOfAdapter(cb, counter) {
+    counter = counter || 0;
+    console.log('Try check #' + counter);
+    if (counter > 30) {
+        if (cb) cb('Cannot check connection');
+        return;
+    }
+
+    console.log('Checking alive key for key : ' + adapterShortName);
+    states.getState('system.adapter.' + adapterShortName + '.0.alive', function (err, state) {
+        if (err) console.error(err);
+        if (state && state.val) {
+            if (cb) cb();
+        } else {
+            setTimeout(function () {
+                checkConnectionOfAdapter(cb, counter + 1);
+            }, 1000);
+        }
+    });
+}
+/*
+function checkValueOfState(id, value, cb, counter) {
+    counter = counter || 0;
+    if (counter > 20) {
+        if (cb) cb('Cannot check value Of State ' + id);
+        return;
+    }
+
+    states.getState(id, function (err, state) {
+        if (err) console.error(err);
+        if (value === null && !state) {
+            if (cb) cb();
+        } else
+        if (state && (value === undefined || state.val === value)) {
+            if (cb) cb();
+        } else {
+            setTimeout(function () {
+                checkValueOfState(id, value, cb, counter + 1);
+            }, 500);
+        }
+    });
+}
+*/
+
+describe('Test ' + adapterShortName + '-writeNulls adapter', function() {
+    before('Test ' + adapterShortName + '-writeNulls adapter: Start js-controller', function (_done) {
+        this.timeout(600000); // because of first install from npm
+
+        setup.setupController(async function () {
+            var config = await setup.getAdapterConfig();
+            // enable adapter
+            config.common.enabled  = true;
+            config.common.loglevel = 'debug';
+
+            config.native.enableDebugLogs = true;
+            //config.native.dbtype   = 'sqlite';
+
+            await setup.setAdapterConfig(config.common, config.native);
+
+            setup.startController(true, function(id, obj) {}, function (id, state) {
+                    if (onStateChanged) onStateChanged(id, state);
+                },
+                async (_objects, _states) => {
+                    objects = _objects;
+                    states  = _states;
+
+                    await tests.preInit(objects, states, sendTo, adapterShortName);
+
+                    _done();
+                });
+        });
+    });
+
+    it('Test ' + adapterShortName + '-writeNulls adapter: Check if adapter started', function (done) {
+        this.timeout(60000);
+        checkConnectionOfAdapter(function (res) {
+            if (res) console.log(res);
+            expect(res).not.to.be.equal('Cannot check connection');
+            done();
+        });
+    });
+
+    tests.register(it, expect, sendTo, adapterShortName, true, 0, 0);
+
+    after('Test ' + adapterShortName + '-writeNulls adapter: Stop js-controller', function (done) {
+        this.timeout(20000);
+
+        setup.stopController(function (normalTerminated) {
+            console.log('Adapter normal terminated: ' + normalTerminated);
+            setTimeout(done, 10000);
+        });
+    });
+});
