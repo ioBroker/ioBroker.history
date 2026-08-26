3a007cfa90793e6f8bff4dbb57860cbb0d728251
GermanBluefox <dogafox@gmail.com>
2026-08-26 21:27:53 +0300

    Fixed average and total

---
+++ admin/custom/customComponents.js
@@ -0,0 +1,12 @@
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__HistoryComponentsSet__customComponents_js-C6Oi0t4w.js";export{t as get,e as init};
+if (typeof document !== 'undefined' && document.head) {
+  try {
+    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__HistoryComponentsSet__customComponents_js-C6Oi0t4w.js","assets/virtualExposes-8qEVV_Eu.js","assets/virtual_mf-exposes___mfe_internal__HistoryComponentsSet__customComponents_js-60vzgxXG.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__HistoryComponentsSet__mf_owner__1-DOAtVaTb.js","assets/vite-preload-helper-B7qeedMF.js"]) {
+      const __mfWarmupLink = document.createElement('link');
+      __mfWarmupLink.rel = 'modulepreload';
+      __mfWarmupLink.crossOrigin = '';
+      __mfWarmupLink.href = new URL(__mfWarmupPath, import.meta.url).href;
+      document.head.appendChild(__mfWarmupLink);
+    }
+  } catch (__mfWarmupError) {}
+}
